f0079d88a6a84eaa7492b0eb8b087451b0c73064
Excel951 <abigailexcelsisdeo@gmail.com>
2023-04-09 23:33:07 +0700

    update tugas

--- latihanWeb/tugas5/getTimes.js
+++ latihanWeb/tugas5/getTimes.js
@@ -1,14 +1,30 @@
-// $.fn.ambilWaktu = function () {
-let dates = new Date();
-let year = dates.getFullYear();
-let month = dates.getMonth() + 1;
-let date = dates.getDate();
-let hour = dates.getHours();
-let minute = dates.getMinutes();
-let seconds = dates.getSeconds();
+// // $.fn.ambilWaktu = function () {
+// let dates = new Date();
+// let year = dates.getFullYear();
+// let month = dates.getMonth() + 1;
+// let date = dates.getDate();
+// let hour = dates.getHours();
+// let minute = dates.getMinutes();
+// let seconds = dates.getSeconds();
 
-const setKode = `${year}${month}${date}${hour}${minute}${seconds}`;
-const setTgl = dates.toLocaleDateString();
-// };
+// const setKode = `${year}${month}${date}${hour}${minute}${seconds}`;
+// const setTgl = dates.toLocaleDateString();
+// // };
 
-export { setKode, setTgl };
+// export { setKode, setTgl };
+
+const boj = [
+	{
+		1: {
+			nama: "asdajksdn",
+		},
+		2: {
+			nama: "akjshdasd",
+		},
+	},
+];
+
+boj.forEach(function () {
+	console.log("i");
+});
+console.log(boj);
